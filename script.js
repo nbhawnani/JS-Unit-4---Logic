b4c7017ea77a0f5ea7271b2ea97cb02794f18c67
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-03-30 13:52:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+/*
+let a=-10, b=50;
+
+if (a>=25){
+  console.log('a is greater than or equal to 25');
+}else if (a<10){
+  console.log('a is less than 10')
+} else{
+  console.log('a is between 10 and 24');
+}
+*/
+
+/*
+>
+<
+>=
+<=
+==
+!=
+=== data value and data type
+!==
+*/
+
+
+
+
+/*
+
+let mike = "false";
+if (mike === true) {
+alert("Mike is in the class");
+} else {
+alert("Mike is absent today");
+}
+
+*/
+/*
+// ==true is implied in the condition 
+
+let a=true; //boolean
+
+if (a){
+  console.log('a is true');
+}else{
+  console.log('a is false');
+}
+*/
+
+
+
+
+let a=75; //number
+
+if (a!=75){
+  console.log('a is not 75');
+}else{
+  console.log('a is 75');
+}
